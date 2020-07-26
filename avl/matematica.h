4a2e0c10460cd4197d27d32f07912ce4909a5421
EmmanuelMess <emmanuelbendavid@gmail.com>
2020-07-26 14:00:10 -0300

    Documentado matematica

--- avl/matematica.h
+++ avl/matematica.h
@@ -1,8 +1,14 @@
 #ifndef ESTRUCTURA_DE_DATOS_I_2020_TPFINAL_MATEMATICA_H
 #define ESTRUCTURA_DE_DATOS_I_2020_TPFINAL_MATEMATICA_H
 
+/**
+ * Funcion simple que calcula el maximo entre dos valores
+ */
 int max(int a, int b);
 
+/**
+ * Funcion simple que calcula el minimo entre dos valores
+ */
 int min(int a, int b);
 
 #endif //ESTRUCTURA_DE_DATOS_I_2020_TPFINAL_MATEMATICA_H
